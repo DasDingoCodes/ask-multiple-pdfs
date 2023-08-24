69f0010e326f6a609f33358b6f145ea347cad6b0
DasDingo <dazdingo90@gmail.com>
2023-08-24 11:20:37 +0200

    chg: app.py local llm

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from langchain.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
+
+import logging
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -18,38 +22,42 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
     return text
 
-
-def get_text_chunks(text):
+def get_text_chunks(raw_text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = text_splitter.split_text(text)
+    chunks = text_splitter.split_text(raw_text)
     return chunks
 
-
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = HuggingFaceInstructEmbeddings(model_name="thenlper/gte-base")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+    if "llm" not in st.session_state:
+        st.session_state.llm = LlamaCpp(
+            model_path="models/wizardlm-1.0-uncensored-llama2-13b.ggmlv3.q2_K.bin",
+            n_gpu_layers=25,
+            n_ctx=2048,
+            f16_kv=True,
+            callback_manager=callback_manager,
+            verbose=True
+        )
+    logging.info("Loaded llm into session state")
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
+        llm=st.session_state.llm,
         retriever=vectorstore.as_retriever(),
-        memory=memory
+        memory=memory,
+        rephrase_question=False
     )
+    logging.info("Loaded conversation_chain")
     return conversation_chain
-
 
 def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
@@ -62,6 +70,7 @@ def handle_userinput(user_question):
         else:
             st.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
+
 
 
 def main():
@@ -100,5 +109,5 @@ def main():
                     vectorstore)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
